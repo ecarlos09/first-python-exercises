5c851510209ed2ef679342218fdd2a86fb24b7b1
Elwin <elwin.carlos09@gmail.com>
2021-05-06 09:38:00 +0100

    add code for final few exercises on python functions

--- my_first_pytthon_funcs.py
+++ my_first_pytthon_funcs.py
@@ -1,1 +1,18 @@
-my_hometown = "Uxbridge"
+my_hometown = "Hillingdon"
+hometown_stats = {
+    "location": 'London',
+    "founded": '01/04/1965',
+    "population": 306870,
+    "area_sq_miles": 44,
+}
+
+def pop_density(population, area):
+    return population/area
+
+def inform_user(hometown="Somewhere", pop_density="undetermined!"):
+    return f"{hometown}'s population density is {pop_density}."
+
+print(inform_user(my_hometown, pop_density(hometown_stats["population"], hometown_stats["area_sq_miles"])))
+    # Will print "Hillingdon's population density is 6974.318181818182."
+print(inform_user())
+    # Will print "Somewhere's population density is undetermined!"
